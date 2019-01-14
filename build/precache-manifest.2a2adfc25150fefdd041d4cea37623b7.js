224f82afb0e691a1fc5400098e1a5fa1e49d6455
Christopher Altamirano <chrisalta94@gmail.com>
2019-01-14 15:29:51 -0500

    Adding project

---
+++ build/precache-manifest.2a2adfc25150fefdd041d4cea37623b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3cea946cd7f659fa7887",
+    "url": "/static/js/main.3cea946c.chunk.js"
+  },
+  {
+    "revision": "b352fff3b5b46aad20a0",
+    "url": "/static/js/1.b352fff3.chunk.js"
+  },
+  {
+    "revision": "3cea946cd7f659fa7887",
+    "url": "/static/css/main.12afdc14.chunk.css"
+  },
+  {
+    "revision": "b352fff3b5b46aad20a0",
+    "url": "/static/css/1.b05ef09f.chunk.css"
+  },
+  {
+    "revision": "8ce7b449bdf4c211c1bdf362bad4156e",
+    "url": "/index.html"
+  }
+];
